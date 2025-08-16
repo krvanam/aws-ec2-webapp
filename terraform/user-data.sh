a66513dbc3e3223a7835e4354eca847f7d54210f
Ubuntu <vanams365@gmail.comXX>
2025-08-16 18:48:46 +0000

    Modified user-data.sh file

--- terraform/user-data.sh
+++ terraform/user-data.sh
@@ -5,7 +5,7 @@ yum install -y python3-pip git
 
 # Clone app repo (replace with your GitHub repo)
 cd /home/ec2-user
-git clone https://github.com/krvanam/aws-ec2-simple-webapp.git
+git clone https://github.com/krvanam/aws-ec2-webapp.git
 cd aws-ec2-simple-webapp/app
 
 pip3 install -r requirements.txt
